eea8527ecc4ba70175e1807a8298c8674d5dace3
xenakios <xenakios@gmail.com>
2023-11-21 20:02:40 +0200

    Initial git commit

---
+++ PluginProcessor.cpp
@@ -0,0 +1,8 @@
+#include "PluginProcessor.h"
+// #include "PluginEditor.h"
+
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new HostAudioProcessor();
+}
